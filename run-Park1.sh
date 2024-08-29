53a2caf056a1c1ba9e5540f7a0c438e7a5e4106f
Tanmay Khandait (Student) <tkhandai@en4230638l.fulton.ad.asu.edu>
2024-08-29 11:48:02 -0700

    changed experimental setup

--- run-Park1.sh
+++ run-Park1.sh
@@ -3,7 +3,7 @@
 python main.py --domain Park1 \
                --inits 20,2 \
                --epochs 5000 \
-               --maxiter 40 \
+               --maxiter 20 \
                --activation relu \
                --hlayers_width 128,128 \
                --hlayers_depth 2,2 \
